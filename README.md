37478cbdb46a67a1a7d350e56a0ea2e1f849bba6
Naresh Kalakanti <naresh.kalakanti@gmail.com>
2024-01-17 14:41:59 +0000

    create eks cluster with Iam roles and node autoscaler

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# spl-terraform-jenkins-eks
-Deploying EKS cluster using Terraform and Jenkins
+# Terraform Jenkins EKS
+
+Deploying EKS cluster using Terraform and IAM Roles for Service Accounts & EKS Cluster Autoscaler
+- Create VPC using Terraform
+- Create IGW AWS using Terraform
+- Create private & public subnets in AWS using TF
+- Creating NAT gatway in AWS using TF
+- Creating EKS cluster using TF
+- Create IAM OIDC provider EKS using TF
+- Create Public LB on EKS
+- Create Private LB on EKS
+- Deploy EKS cluster Autoscaler
+
---
+++ eks/3-subnets.tf
@@ -0,0 +1,48 @@
+resource "aws_subnet" "private-us-west-2a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name                              = "private-us-west-2a"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+resource "aws_subnet" "private-us-west-2b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    "Name"                            = "private-us-west-2b"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+# map launch = true will create public k8s instance groups and Each k8s worker will get public IP which is not needed
+resource "aws_subnet" "public-us-west-2a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/19"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "public-us-west-2a"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+
+resource "aws_subnet" "public-us-west-2b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.96.0/19"
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "public-us-west-2b"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
